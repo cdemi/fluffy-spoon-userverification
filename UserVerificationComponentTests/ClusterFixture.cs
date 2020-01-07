133f0fb8284a19f9841eb2c356636cac97bffbe2
Joseph Marmara <joseph.marmara@gig.com>
2020-01-07 17:36:49 +0100

    Added stream test.

---
+++ UserVerificationComponentTests/ClusterFixture.cs
@@ -0,0 +1,75 @@
+﻿using demofluffyspoon.contracts;
+using fluffyspoon.userverification.Grains;
+using GiG.Core.Data.KVStores.Abstractions;
+using GiG.Core.Data.KVStores.Extensions;
+using GiG.Core.Data.KVStores.Providers.FileProviders.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Orleans;
+using Orleans.Hosting;
+using Orleans.TestingHost;
+using System;
+using System.Collections.Generic;
+
+namespace UserVerificationComponentTests
+{
+    public class ClusterFixture : IDisposable
+    {
+        public TestCluster Cluster { get; }
+
+        public ClusterFixture()
+        {
+            var builder = new TestClusterBuilder();
+            builder.AddSiloBuilderConfigurator<TestSiloConfigurations>();
+            builder.AddClientBuilderConfigurator<TestClientConfigurations>();
+
+            Cluster = builder.Build();
+            Cluster.Deploy();
+        }
+
+        public void Dispose() => Cluster.StopAllSilos();
+    }
+    
+    public class TestSiloConfigurations : ISiloBuilderConfigurator {
+        private IHost _host;
+
+        public TestSiloConfigurations()
+        {
+            _host = new HostBuilder()
+                .ConfigureServices((ctx, services) =>
+                    {
+                        services
+                            .AddKVStores<HashSet<string>>()
+                            .FromJsonFile(ctx.Configuration.GetSection("BlacklistedEmails"))
+                            .AddMemoryDataStore();
+                    })
+                .ConfigureAppConfiguration(x => x.AddJsonFile("appsettings.json")).Build();
+            _host.Start();
+        }
+
+        public void Configure(ISiloHostBuilder hostBuilder)
+        {
+            hostBuilder.AddSimpleMessageStreamProvider(Constants.StreamProviderName,
+                    options => options.FireAndForgetDelivery = false)
+                .AddMemoryGrainStorage("PubSubStore")
+                .ConfigureApplicationParts(parts =>
+                {
+                    parts.AddApplicationPart(typeof(UserVerificationGrain).Assembly).WithReferences();
+                })
+                .ConfigureServices((ctx, services) =>
+                {
+                    services.AddSingleton(x => _host.Services.GetService<IDataRetriever<HashSet<string>>>());
+                });
+        }
+    }
+    
+    public class TestClientConfigurations : IClientBuilderConfigurator {
+        public void Configure(IConfiguration configuration, IClientBuilder clientBuilder)
+        {
+            clientBuilder.AddSimpleMessageStreamProvider(Constants.StreamProviderName, options => options.FireAndForgetDelivery = false);
+        }
+    }
+    
+}
+
---
+++ UserVerificationComponentTests/UserVerificationTests.cs
@@ -0,0 +1,67 @@
+﻿using demofluffyspoon.contracts;
+using demofluffyspoon.contracts.Models;
+using Orleans.Streams;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UserVerificationComponentTests
+{
+    public class UserVerificationTests: IClassFixture<ClusterFixture>, IAsyncObserver<UserVerifiedEvent>
+    {
+        private readonly ClusterFixture _cluster;
+        private UserVerifiedEvent _userVerifiedEvent;
+        private readonly SemaphoreSlim _semaphore;
+        
+        public UserVerificationTests(ClusterFixture fixture)
+        {
+            _cluster = fixture;
+            _userVerifiedEvent = null;
+            _semaphore = new SemaphoreSlim(0, 1);
+        }
+
+        [Fact]
+        public async Task UserVerification_DuplicateEmail_NotProcessed()
+        {
+            Guid testGuid = Guid.NewGuid();
+
+
+            var streamProvider = _cluster.Cluster.Client.GetStreamProvider(Constants.StreamProviderName);
+            var userRegistrationStream = streamProvider.GetStream<UserRegisteredEvent>(testGuid, nameof(UserRegisteredEvent));
+            var userVerifiedStream = streamProvider.GetStream<UserVerifiedEvent>(testGuid, nameof(UserVerifiedEvent));
+
+
+            await userVerifiedStream.SubscribeAsync(this);
+            await userRegistrationStream.OnNextAsync(new UserRegisteredEvent() {Email = "testing@test.com", Name = "test1", Surname = "test2"});
+            _semaphore.Wait(10000);
+
+            Assert.Equal("testing@test.com", _userVerifiedEvent.Email);
+            _userVerifiedEvent = null;
+            
+            await userRegistrationStream.OnNextAsync(new UserRegisteredEvent() {Email = "testing@test.com", Name = "test1", Surname = "test2"});
+
+            _semaphore.Wait(10000);
+
+            Assert.Equal("testing@test.com", _userVerifiedEvent.Email);
+        }
+
+        public Task OnNextAsync(UserVerifiedEvent item, StreamSequenceToken token = null)
+        {
+            _userVerifiedEvent = item;
+            _semaphore.Release();
+            
+            return Task.CompletedTask;
+        }
+
+        public Task OnCompletedAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task OnErrorAsync(Exception ex)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
